dadd4a576c3d11feefcea841fc3a12b73da5dff9
connor alsop (home) <connoralsop@gmail.com>
2023-06-22 11:49:25 +0100

    Abstraction practice

--- practice/OOPractice.cpp
+++ practice/OOPractice.cpp
@@ -1,2 +1,51 @@
 #include "Libaries.cpp"
 
+class AbstractEmployee {//abstract class to define rules
+	virtual void AskForPromotion() =0;
+
+
+};
+
+class Employee:AbstractEmployee {//inhetiting abstract class means required to have functions in abstarct class
+private://encapsulation
+	string Name;
+	string Company;
+	int Age;
+
+public:
+	Employee(string name, string company, int age) {
+		Name = name;
+		Company = company;
+		Age = age;
+	}
+
+	void IntroduceYourself() {
+		cout << "Hello my name is " << Name << " I work for " << Company << " and am " << Age << " years old." << endl;
+	}
+	void setName(string name) {//setter
+		Name = name;
+	}
+	string getName() {//getter
+		return Name;
+	}
+	void setAge(int age) {//setter
+		if (age > 18) { Age = age; }
+		else { cout << "Employees cannot be younger that 18 years old." << endl; };
+		
+	}
+	int getAge() {//getter
+		return Age;
+	}
+
+	void AskForPromotion() {
+		if (Age>30)
+		{
+			cout << Name << " got promoted!" << endl;
+		}
+		else
+		{
+			cout << Name << " did not got promoted." << endl;
+		}
+
+	}
+};
--- practice/Source.cpp
+++ practice/Source.cpp
@@ -1,6 +1,10 @@
+
+
 #include "LeetCode.cpp"
-#include "GeneralPractice.cpp"
+//#include "GeneralPractice.cpp" // build error after reorganising needs fix
 #include "OOPractice.cpp"
+
+
 
 int  main()
 {
@@ -11,8 +15,19 @@ int  main()
  cout << Test.searchInsert(nums, 6) << endl;*/
 
 	//OO practice
+	Employee employee1("name1", "company1", 25);
+	Employee employee2("name2", "company2", 35);
 
+	employee1.IntroduceYourself();
+	employee2.IntroduceYourself();
 
-	
+	employee1.setAge(15);
+	cout << "age 15 test: " << employee1.getAge() << endl;
+	employee1.AskForPromotion();
+
+	employee1.setAge(50);
+	cout << "age 50 test: " << employee1.getAge() << endl;
+	employee1.AskForPromotion();
+
 	return 0;
 }
