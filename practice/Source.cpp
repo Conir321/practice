591d094492db62172877c6556df4ec7593468c2c
connor alsop (home) <connoralsop@gmail.com>
2023-06-28 13:25:23 +0100

    More pointer practice (inc void pointers)

--- practice/Source.cpp
+++ practice/Source.cpp
@@ -4,16 +4,42 @@
 //#include "GeneralPractice.cpp" // build error after reorganising needs fix
 #include "OOPractice.cpp"
 
+void printPointer(void* ptr,char type) {
+	switch (type)
+	{
+	case 'i':cout << *(int*)ptr << endl; break;
+	case 'c':cout << *(char*)ptr << endl; break;
+	default:
+		cout << "printPointer error: no case selected." << endl;
+		break;
+	}
+	
+}
 
+void pointerPractice() {
+	int number = 5;
+	int* ptrNumber = &number;
+
+	char letter = 'a';
+	char* ptrLetter = &letter;
+
+	printPointer(ptrNumber,'i');
+	printPointer(ptrLetter,'c');
+	printPointer(ptrNumber, '6');
+
+	
+}
 
 int  main()
 {
 
 	//leet code
-	Solution Test;
+	//Solution Test;
 	//vector<int> nums = { 1,4,6,7,8,9 };
- cout << "word length: "<<Test.lengthOfLastWord("a") << endl;
+// cout << "word length: "<<Test.lengthOfLastWord("a") << endl;
 
+	pointerPractice();
+	
 
 
 	return 0;
